22be78d6e44f2757a4445a05f1ab66e1de3decc7
rahulpihub <rahul.p.ihub@snsgroups.com>
2024-11-11 17:33:21 +0530

    messge  added to api folder

--- api/send-email.js
+++ api/send-email.js
@@ -14,11 +14,27 @@ export default async function handler(req, res) {
     });
 
     const mailOptions = {
-      from: process.env.YOUR_EMAIL,
-      to: email,
-      subject: 'Your Call Code',
-      text: `Your unique call code is: ${code}`,
-    };
+        from: process.env.YOUR_EMAIL,
+        to: email,
+        subject: 'Final HR Interview - Access Link and Call Code',
+        text: `Dear Candidate,
+    
+    We are pleased to invite you to the next stage of our hiring process—a virtual HR interview. Please review the instructions below to ensure a smooth and successful meeting.
+    
+    Interview Details:
+    Link to Join: https://interview-taupe-nine.vercel.app/
+    Unique Call Code: ${code}
+    Instructions:
+    At your scheduled interview time, click the link provided above to open the interview platform.
+    Enter Your Unique Call Code: You will be prompted to enter your unique code (${code}). This code connects you directly with our HR representative.
+    Complete Your Setup: Ensure your camera and microphone are enabled to allow for video and audio during the interview.
+    Please be ready and logged in at the scheduled time. If you encounter any issues connecting, feel free to reach out to us immediately for assistance.
+    
+    Thank you for your interest and preparation. We look forward to our conversation!
+    
+    Best regards,
+    HR TEAM`,
+      };
 
     try {
       await transporter.sendMail(mailOptions);
